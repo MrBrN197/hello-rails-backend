cd77a521a0fe4f75187896378563f13e90795346
brianorora <brianorora@gmail.com>
2022-02-15 15:36:12 +0300

    Fix: linter errors

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,9 @@
-class Api::MessagesController < ApplicationController
-  def index
-    render json: { message: Message.random }
+# frozen_string_literal: true
+
+module Api
+  class MessagesController < ApplicationController
+    def index
+      render json: { message: Message.random }
+    end
   end
 end
